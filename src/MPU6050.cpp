7b2130aaf3b173431225a17115e21ee550497258
Andrzej Ziębiński <35402510+Reds-Labs@users.noreply.github.com>
2022-07-18 02:47:27 +0200

    Add files via upload

--- src/MPU6050.cpp
+++ src/MPU6050.cpp
@@ -1,6 +1,6 @@
 #include "MPU6050.h"
 #include <Wire.h>
-
+#include <Arduino.h>
 MPU6050::MPU6050(){}
 
 bool MPU6050::I2Cread(uint8_t addr,  uint8_t* data,  uint8_t len) {
@@ -20,13 +20,12 @@ bool MPU6050::I2Cwrite(uint8_t addr,  uint8_t* data, uint8_t len) {
 	uint8_t pos = 0;
 	Wire.beginTransmission(addr);
 	Wire.write(data, len);
-	pos = Wire.endTransmission();
+	pos = Wire.endTransmission(false);
 
     return pos == 0;
 }
 
 bool MPU6050::begin() {
-
 	int8_t data[2];
 	bool status = 1;
 
@@ -41,8 +40,8 @@ bool MPU6050::begin() {
 	data[1]=DEFAULT_CONFIG;
 	I2Cwrite(MPU_ADDR, data, 2);
 
-	data[0]=0x19;
-	data[1]=0x07;
+	data[0]=SMPRT_DIV;
+	data[1]=GYRO_ACL_SYNC;
 	I2Cwrite(MPU_ADDR, data, 2);
 
 	data[0]=GYRO_CONFIG;
@@ -55,23 +54,119 @@ bool MPU6050::begin() {
 	return status;
 }
 
-bool MPU6050::getData(int16_t* array, uint8_t len) {
+void MPU6050::setSettings(uint8_t powerMode, uint8_t mpuConfig, uint8_t gyroSmplRt) {
+	uint8_t data[2];
+	
+	data[0]=PWR_MGMT_1;
+	data[1]=powerMode;
+	I2Cwrite(MPU_ADDR, data, 2);
+
+	data[0]=CONFIG;
+	data[1]=mpuConfig;
+	I2Cwrite(MPU_ADDR, data, 2);
+
+	data[0]=SMPRT_DIV;
+	data[1]=gyroSmplRt;
+	I2Cwrite(MPU_ADDR, data, 2);
+
+	data[0]=GYRO_CONFIG;
+	data[1]=0x00;
+	I2Cwrite(MPU_ADDR, data, 2);
+
+	data[0]=ACCEL_CONFIG;
+	data[1]=0x00;
+	I2Cwrite(MPU_ADDR, data, 2);
+}
+
+void MPU6050::setPrescaler(uint8_t aclPrescaler, uint8_t gyroPrescaler) {
+	uint8_t data[2];
+	data[0]=GYRO_CONFIG;
+	data[1]=gyroPrescaler<<3;
+	I2Cwrite(MPU_ADDR, data, 2);
+
+	data[0]=ACCEL_CONFIG;
+	data[1]=aclPrescaler<<3;
+	I2Cwrite(MPU_ADDR, data, 2);
+
+	//aclScale = (float)(CONVERSION_CONSTANT_ACL>>aclPrescaler);
+	switch(gyroPrescaler) {
+		case 0:
+			gyroScale = 131.0;
+			break;
+		case 1:
+			gyroScale = 65.5;
+			break;
+		case 2:
+			gyroScale = 32.8;
+			break;
+		case 3:
+			gyroScale = 16.4;
+			break;
+	}
+}
+
+bool MPU6050::getRaw(int16_t* array, uint8_t len) {
 	bool status;
 	if(len<6) {return 0;}
-	int8_t input[14];
+	uint8_t input[14];
 
 	I2Cwrite(MPU_ADDR, &ACC_ADR, 1);
 	status = I2Cread(MPU_ADDR, input, 14);
 
-	array[0] = ((int16_t)input[0]) << 8 | input[1];
-	array[1] = ((int16_t)input[2]) << 8 | input[3];
-	array[2] = ((int16_t)input[4]) << 8 | input[5];
+	array[0] = (int16_t)(((uint16_t)input[0]) << 8 | input[1]);
+	array[1] = (int16_t)(((uint16_t)input[2]) << 8 | input[3]);
+	array[2] = (int16_t)(((uint16_t)input[4]) << 8 | input[5]);
 
-	array[3] = ((int16_t)input[8]) << 8 | input[9];
-	array[4] = ((int16_t)input[10]) << 8 | input[11];
-	array[5] = ((int16_t)input[12]) << 8 | input[13];
+	array[3] = (int16_t)(((uint16_t)input[8]) << 8 | input[9]);
+	array[4] = (int16_t)(((uint16_t)input[10]) << 8 | input[11]);
+	array[5] = (int16_t)(((uint16_t)input[12]) << 8 | input[13]);
 
 	return status;
 }
 
+void MPU6050::calibrate(uint8_t samples) {
+	float data[6], newOffset[6] = {0,0,0,0,0,0};
+	offsets[0] = 0.0;
+	offsets[1] = 0.0;
+	offsets[2] = 0.0;
+	offsets[3] = 0.0;
+	offsets[4] = 0.0;
+	offsets[5] = 0.0;
+	for(uint8_t i = 0; i <samples; i++) {
+		getData(data, 6);
 
+		newOffset[0] += data[0];
+		newOffset[1] += data[1];
+		newOffset[2] += data[2]-1.0;
+		newOffset[3] += data[3];
+		newOffset[4] += data[4];
+		newOffset[5] += data[5];
+		delay(2);
+	}
+
+	offsets[0] = newOffset[0]/(float)samples;
+	offsets[1] = newOffset[1]/(float)samples;
+	offsets[2] = newOffset[2]/(float)samples;
+	offsets[3] = newOffset[3]/(float)samples;
+	offsets[4] = newOffset[4]/(float)samples;
+	offsets[5] = newOffset[5]/(float)samples;
+
+
+}
+
+bool MPU6050::getData(float* array, uint8_t len) {
+	
+	if(len<6) return 0;
+	
+	int16_t data[6];
+	bool status = getRaw(data, 6);
+
+	array[0] = ((float)data[0])/aclScale - offsets[0];
+	array[1] = ((float)data[1])/aclScale - offsets[1];
+	array[2] = ((float)data[2])/aclScale - offsets[2];
+	array[3] = ((float)data[3])/gyroScale - offsets[3];
+	array[4] = ((float)data[4])/gyroScale - offsets[4];
+	array[5] = ((float)data[5])/gyroScale - offsets[5];
+
+	return status;
+}
--- src/MPU6050.h
+++ src/MPU6050.h
@@ -10,20 +10,26 @@ class MPU6050 {
 
         inline static const uint8_t WHO_AM_I			= 0x75;
         inline static const uint8_t CONFIG				= 0x1A;
-        inline static const uint8_t GYRO_CONFIG		= 0x1B;
+        inline static const uint8_t GYRO_CONFIG		    = 0x1B;
         inline static const uint8_t ACCEL_CONFIG		= 0x1C;
+        inline static const uint8_t SMPRT_DIV           = 0x19;
 
-        inline static const uint8_t ACC_ADR			= 0x3B;
-        inline static const uint8_t GYR_ADR			= 0x43;
+        inline static const uint8_t ACC_ADR			    = 0x3B;
+        inline static const uint8_t GYR_ADR			    = 0x43;
         inline static const uint8_t PWR_MGMT_1			= 0x6B;
 
         inline static const uint8_t DEFAULT_CONFIG 	= 0b00000110;
         inline static const uint8_t DEFAULT_ACL		= 0b00000000;
         inline static const uint8_t DEFAULT_GYR		= 0b00000000;
+        inline static const uint8_t GYRO_ACL_SYNC   = 0x07;
+
+        float gyroScale = 131.0, aclScale = 16384.0;
 
         bool I2Cwrite(uint8_t addr, uint8_t* data, uint8_t len);
 
         bool I2Cread(uint8_t addr, uint8_t* data, uint8_t len);
+
+        float offsets[6];
 
     public:
 
@@ -31,6 +37,14 @@ class MPU6050 {
 
         bool begin();
 
-        bool getData(int16_t* array, uint8_t len);
+        void setSettings(uint8_t powerMode, uint8_t aclConf, uint8_t gyroSmplRt);
+
+        void setPrescaler(uint8_t aclPrescaler, uint8_t gyroPrescaler);
+
+        void calibrate(uint8_t samples);
+
+        bool getRaw(int16_t* array, uint8_t len);
+
+        bool getData(float* array, uint8_t len);
 };
 #endif /* MPU6050_H_ */
